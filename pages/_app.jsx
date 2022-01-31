2d2944e070ff5ff53370ab78b766512bfb7b6dc4
felixHerder <felixbaciu@gmail.com>
2022-01-31 19:53:13 +0200

    added ga pageview tracking

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,11 +1,47 @@
-import '@fontsource/overpass/200.css'
-import '@fontsource/overpass/400.css'
-import '@fontsource/overpass/700.css'
-import '@fontsource/pt-sans/400.css'
-import '@fontsource/pt-sans/700.css'
+import "@fontsource/overpass/200.css";
+import "@fontsource/overpass/400.css";
+import "@fontsource/overpass/700.css";
+import "@fontsource/pt-sans/400.css";
+import "@fontsource/pt-sans/700.css";
+import { useRouter } from "next/router";
+import Script from "next/script";
+import { useEffect } from "react";
+import * as gtag from '../lib/gtag'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  //for google analytics
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
+  return (
+    <>
+    {/* for google analytics */}
+      <Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_ID}`} />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
